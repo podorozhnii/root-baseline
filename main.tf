59205d0c4743881f0fe71cbb40a447beb9705c13
podorozhnii <podorojnii@gmail.com>
2021-04-22 00:04:30 +0300

    change bucket_name

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 variable "bucket_name" {
 type=string
-description = "Bucket name"
+description = "Bucket name for Alex"
 }
 resource "aws_s3_bucket" "test-bucket" {
 bucket = var.bucket_name
