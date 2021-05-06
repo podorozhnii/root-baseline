dafb66871c0f3d8187c9b1b63a4b9f41e3fc069e
podorozhnii <podorojnii@gmail.com>
2021-05-06 23:58:01 +0300

    remote state S3 with Dynamo_DB

---
+++ 0-preinstall/variables.tf
@@ -0,0 +1,11 @@
+variable "bucket_state_name" {
+  type = string
+  description = "Terraform remote state storage S3 bucket name"
+
+}
+
+variable "state_lock_name" {
+  type = string
+  description = "Terraform remote state storage S3 bucket name"
+
+}
